7ed75563721da2d6cb8b95ffc407b7b11d242d9d
PILSUNyou <dbyo1125@naver.com>
2024-07-15 18:40:26 +0900

    new project

---
+++ src/main/java/com/example/Trip_In_Jeju/TripInJejuApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trip_In_Jeju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripInJejuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripInJejuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Trip_In_Jeju/TripInJejuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trip_In_Jeju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripInJejuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

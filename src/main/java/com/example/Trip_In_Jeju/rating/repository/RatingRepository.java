e4ecec9a3d98312949c7c74147a0b16e2b5f9225
PILSUNyou <dbyo1125@naver.com>
2024-07-24 18:05:04 +0900

    main 수정

--- src/main/java/com/example/Trip_In_Jeju/rating/repository/RatingRepository.java
+++ src/main/java/com/example/Trip_In_Jeju/rating/repository/RatingRepository.java
@@ -3,9 +3,11 @@ package com.example.Trip_In_Jeju.rating.repository;
 import com.example.Trip_In_Jeju.rating.entity.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     @Query("SELECT AVG(r.score) FROM Rating r WHERE r.dessertId = ?1")
     Double findAverageScoreByDessertId(Long dessertId);

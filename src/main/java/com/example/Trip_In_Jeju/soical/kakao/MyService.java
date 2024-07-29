edc7eba4ea2338050544a8f9f9a31654196766b4
PILSUNyou <dbyo1125@naver.com>
2024-07-29 17:33:55 +0900

    test2

--- src/main/java/com/example/Trip_In_Jeju/soical/kakao/MyService.java
+++ src/main/java/com/example/Trip_In_Jeju/soical/kakao/MyService.java
@@ -1,11 +1,9 @@
 package com.example.Trip_In_Jeju.soical.kakao;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class MyService {
 
     @Value("${kakao.map.appkey}")

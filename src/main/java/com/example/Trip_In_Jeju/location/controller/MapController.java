564a17b49a0aa7094576f92c69f030188f72b62a
PILSUNyou <dbyo1125@naver.com>
2024-08-05 14:39:38 +0900

    메인 레이아웃 변경

--- src/main/java/com/example/Trip_In_Jeju/location/controller/MapController.java
+++ src/main/java/com/example/Trip_In_Jeju/location/controller/MapController.java
@@ -3,7 +3,6 @@ package com.example.Trip_In_Jeju.location.controller;
 import com.example.Trip_In_Jeju.location.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,9 +13,8 @@ public class MapController {
 
     private final LocationService locationService; // MyService를 주입받음
 
-    @GetMapping("/map")
-    public String showMap(Model model) {
-        model.addAttribute("kakaoApiKey", locationService.getApiKey());
-        return "map/map";
+    @GetMapping
+    public String showMap() {
+        return "map/map"; // map.html 파일을 반환
     }
 }

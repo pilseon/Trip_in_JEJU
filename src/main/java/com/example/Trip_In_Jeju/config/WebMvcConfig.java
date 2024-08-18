b25b646785df67852fddf73c0232a4c1250591a9
PILSUNyou <dbyo1125@naver.com>
2024-08-18 18:40:18 +0900

    localhost 수정

--- src/main/java/com/example/Trip_In_Jeju/config/WebMvcConfig.java
+++ src/main/java/com/example/Trip_In_Jeju/config/WebMvcConfig.java
@@ -23,7 +23,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8010") // 클라이언트의 URL (배포 시에는 도메인 설정 필요)
+                .allowedOrigins("https://kkyul.site") // 클라이언트의 URL (배포 시에는 도메인 설정 필요)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

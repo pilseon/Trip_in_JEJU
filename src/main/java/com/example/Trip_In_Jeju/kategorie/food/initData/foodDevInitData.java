f27dbd8999e20f89d08ceaa5ab0a94e9b3b1b0d3
PILSUNyou <dbyo1125@naver.com>
2024-08-28 15:12:48 +0900

    food init data test 5

--- src/main/java/com/example/Trip_In_Jeju/kategorie/food/initData/foodDevInitData.java
+++ src/main/java/com/example/Trip_In_Jeju/kategorie/food/initData/foodDevInitData.java
@@ -33,7 +33,7 @@ public class foodDevInitData implements BeforeIntiData {
 
                 // 여러 이미지 파일 경로 설정 (프로젝트 내부 경로로 수정)
                 String[] imagePaths = {
-                        "/images/food/sister.jpeg"
+                        "static/images/food/sister.jpeg"
                 };
 
                 // 파일 경로를 절대 경로로 변경하여 로깅

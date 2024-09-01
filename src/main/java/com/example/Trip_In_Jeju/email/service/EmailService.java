f3520f3d8c4182033cf4192c6d3e3cdb38f9e0bd
PILSUNyou <dbyo1125@naver.com>
2024-09-01 12:11:21 +0900

    test2

--- src/main/java/com/example/Trip_In_Jeju/email/service/EmailService.java
+++ src/main/java/com/example/Trip_In_Jeju/email/service/EmailService.java
@@ -3,14 +3,17 @@ package com.example.Trip_In_Jeju.email.service;
 
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
     private  final JavaMailSender mailSender;
 
@@ -19,19 +22,22 @@ public class EmailService {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
 
         try {
-
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
             mimeMessageHelper.setTo(to); // 메일 수신자
             mimeMessageHelper.setSubject(subject); // 메일 제목
-            mimeMessageHelper.setText(body, true); // 메일 본문 내용 , HTML 여부
+            mimeMessageHelper.setText(body, true); // 메일 본문 내용, HTML 여부
             mailSender.send(mimeMessage); // 메일 발송
-
-        } catch(MessagingException e) {
-            throw new RuntimeException(e);
+        } catch (MailAuthenticationException e) {
+            log.error("메일 인증 실패: {}", e.getMessage());
+            throw e; // 예외를 다시 던져 호출자에게 알림
+        } catch (MailException e) {
+            log.error("메일 전송 실패: {}", e.getMessage());
+            throw e; // 예외를 다시 던져 호출자에게 알림
         } catch (jakarta.mail.MessagingException e) {
             throw new RuntimeException(e);
         }
     }
+
     public void sendVerificationCode(String to, String subject, String body, String verificationCode) {
         String bodyWithCode = body + "\n인증 코드: " + verificationCode;
         send(to, subject, bodyWithCode);
